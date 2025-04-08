3e3311a8b7076906dfc4537b8ab077917336e38a
Aleks <aleks.bitkov.9@gmail.com>
2025-04-08 12:26:39 +0300

    Add project files.

---
+++ Test4/Data/DbRegistrator.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Test4.Data
+{
+    static class DbRegistrator
+    {
+        public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration configuration) => services
+            //.AddDatabase
+            ;
+    }
+}
